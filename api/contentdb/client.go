d14445259f6904c5f2b9568152baa7437bb4edc3
Ronoaldo JLP <ronoaldo@gmail.com>
2021-08-21 01:42:01 +0000

    Added new command for update mods.

--- api/contentdb/client.go
+++ api/contentdb/client.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/ronoaldo/minetools/api"
 )
@@ -28,17 +30,29 @@ func NewClient(ctx context.Context) *Client {
 }
 
 func (c *Client) makeCall(method, path string, query url.Values, body io.Reader) (*http.Response, error) {
-	url := host + path + "?" + query.Encode()
-	api.Debugf("request %v %v", method, url)
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return nil, err
+	var retryCount int64
+	for {
+		url := host + path + "?" + query.Encode()
+		api.Debugf("Request %v %v", method, url)
+		req, err := http.NewRequest(method, url, nil)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("User-Agent", "minetools/go1")
+		resp, err := http.DefaultClient.Do(req)
+		api.Debugf("Response %v: %v", resp.StatusCode, resp.Status)
+		if resp.StatusCode == 429 {
+			retryCount += 1
+			time.Sleep(time.Duration(2*retryCount) * time.Second)
+			continue
+		}
+		if resp.StatusCode == 404 {
+			return nil, fmt.Errorf("contentdb: package not found")
+		}
+		if resp.StatusCode < 200 || resp.StatusCode > 299 {
+			b, _ := ioutil.ReadAll(resp.Body)
+			return nil, fmt.Errorf("contentdb: error calling API, status=%v, payload=%v", resp.StatusCode, string(b))
+		}
+		return resp, nil
 	}
-	req.Header.Set("User-Agent", "minetools/0.0.1")
-	resp, err := http.DefaultClient.Do(req)
-	api.Debugf("response %v: %v", resp.StatusCode, resp.Status)
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return nil, fmt.Errorf("contentdb: internal server error")
-	}
-	return resp, nil
 }
--- cmd/contentdb/main.go
+++ cmd/contentdb/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"text/tabwriter"
 
+	"github.com/fatih/color"
 	"github.com/ronoaldo/minetools/api"
 	"github.com/ronoaldo/minetools/api/contentdb"
 	"github.com/urfave/cli"
@@ -16,8 +18,16 @@ import (
 )
 
 var (
-	apiDebug      bool
-	updatePackage bool
+	apiDebug         bool
+	removeOldpackage bool
+	dryRun           bool
+)
+
+// Helper functions
+var (
+	warnf  = color.New(color.FgHiRed, color.Bold).PrintfFunc()
+	green  = color.New(color.FgGreen, color.Bold).PrintfFunc()
+	yellow = color.New(color.FgYellow, color.Bold).PrintfFunc()
 )
 
 func init() {
@@ -28,30 +38,34 @@ func init() {
 func main() {
 	app := cli.NewApp()
 	app.Name = "contentdb"
-	app.Usage = "Minetest ContentDB client implementation for headless server administration."
+	app.Usage = "Minetest ContentDB client implementation for headless server administration"
 	app.Commands = []cli.Command{
-		{
-			Name:  "install",
-			Usage: "install new content",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:        "update",
-					Usage:       "update if the mod is already installed",
-					Destination: &updatePackage,
-				},
-			},
-			Subcommands: []cli.Command{
-				{
-					Name:   "mod",
-					Usage:  "install a new mod",
-					Action: installMod,
-				},
-			},
-		},
 		{
 			Name:   "search",
 			Usage:  "search for content",
 			Action: search,
+		},
+		{
+			Name:  "install",
+			Usage: "installs new mod into ./mods folder",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:        "update",
+					Usage:       "update if the mod is already installed, removing old contents",
+					Destination: &removeOldpackage,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				return installMod(c.Args())
+			},
+		},
+		{
+			Name:  "update",
+			Usage: "updates all mods in the ./mods folder",
+			Action: func(c *cli.Context) error {
+				removeOldpackage = true
+				return update()
+			},
 		},
 	}
 	app.Flags = append(app.Flags, cli.BoolFlag{
@@ -59,6 +73,11 @@ func main() {
 		EnvVar:      "CDB_DEBUG",
 		Usage:       "show debug information on console",
 		Destination: &apiDebug,
+	})
+	app.Flags = append(app.Flags, cli.BoolFlag{
+		Name:        "dry-run",
+		Usage:       "do not actually perform any opertaions, just report what would be done",
+		Destination: &dryRun,
 	})
 	app.Before = func(c *cli.Context) error {
 		if apiDebug {
@@ -68,7 +87,7 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		api.Warningf("unexpected error: %v", err)
+		warnf("unexpected error: %v", err)
 		os.Exit(1)
 	}
 }
@@ -83,17 +102,16 @@ func search(c *cli.Context) error {
 	}
 	api.Debugf("search: found %d packages", len(pkgs))
 	t := tabwriter.NewWriter(os.Stdout, 18, 0, 1, ' ', 0)
-	fmt.Fprintf(t, "Key\tType\tTitle\tShort description\n")
+	fmt.Fprintf(t, "Key\tType\tTitle\tRelease\tShort description\n")
 	for _, pkg := range pkgs {
-		fmt.Fprintf(t, "%s/%s\t%s\t%s\t%s\n",
-			pkg.Author, pkg.Name, pkg.Type, pkg.Title, fmt.Sprintf("%.60s", pkg.ShortDescription))
+		fmt.Fprintf(t, "%s/%s\t%s\t%s\t%d\t%s\n",
+			pkg.Author, pkg.Name, pkg.Type, pkg.Title, pkg.Release, fmt.Sprintf("%.60s", pkg.ShortDescription))
 	}
 	t.Flush()
 	return nil
 }
 
-func installMod(c *cli.Context) error {
-	mods := c.Args()
+func installMod(mods []string) error {
 	cdb := contentdb.NewClient(context.Background())
 
 	for _, mod := range mods {
@@ -103,7 +121,7 @@ func installMod(c *cli.Context) error {
 		)
 
 		if strings.Count(mod, "/") != 1 {
-			api.Warningf("install: provide a valid package key: author/name (like rubenwardy/sfinv)")
+			warnf("install: provide a valid package key: author/name (like rubenwardy/sfinv)")
 			continue
 		}
 
@@ -112,7 +130,7 @@ func installMod(c *cli.Context) error {
 		s := strings.Split(mod, "/")
 		pkg, err = cdb.GetPackage(s[0], s[1])
 		if err != nil {
-			api.Warningf("install: unable to find %v", mod)
+			warnf("install: unable to find %v", mod)
 			continue
 		}
 
@@ -125,7 +143,7 @@ func installMod(c *cli.Context) error {
 
 		pkgType := archive.Type()
 		if pkgType != contentdb.Mod && pkgType != contentdb.Modpack {
-			api.Warningf("install: package is not a mod/modpack: %s", pkgType)
+			warnf("install: package is not a mod/modpack: %s", pkgType)
 			continue
 		}
 
@@ -177,7 +195,7 @@ func installMod(c *cli.Context) error {
 		// Avoid overwrite destination directory, if updatePackage is not provided.
 		destdir := filepath.Join("mods", modName)
 		if _, err = os.Stat(destdir); !os.IsNotExist(err) {
-			if !updatePackage {
+			if !removeOldpackage {
 				return fmt.Errorf("install: %v already exists, exiting (err=%v)", destdir, err)
 			}
 			fmt.Println("Removing previous installation (--update provided) ...")
@@ -202,7 +220,7 @@ func installMod(c *cli.Context) error {
 			// Sanity check: verify target to prevent Zip Slip vul.
 			target := filepath.Clean(filepath.Join(destdir, fname))
 			if !strings.HasPrefix(target, destdir) {
-				api.Warningf("possible Zip Slip found, ignoring %v", f)
+				warnf("possible Zip Slip found, ignoring %v", f)
 				continue
 			}
 			api.Debugf("Extracting '%v' into '%v'", f, target)
@@ -217,11 +235,90 @@ func installMod(c *cli.Context) error {
 				return fmt.Errorf("install: error writing to %v: %v", target, err)
 			}
 		}
-		fmt.Printf("Intalled %v into %v\n", mod, destdir)
+		green("Intalled %v into %v\n", mod, destdir)
 		fmt.Printf("Add load_mod_%s = true to world.mt to use it.\n", pkg.Name)
 		fmt.Printf("* Dependencies: %v\n", cfg.Key("depends").String())
 		fmt.Printf("* Optional dependencies: %v\n", cfg.Key("optional_depends").String())
 	}
 
+	return nil
+}
+
+func update() error {
+	cdb := contentdb.NewClient(context.Background())
+	mods, err := filepath.Glob("mods/*")
+	if err != nil {
+		warnf("update: error loading mods directory: %v", err)
+		return err
+	}
+
+	fmt.Printf("Updating %d mods in ./mods ...\n", len(mods))
+	for _, mod := range mods {
+		if err := updateMod(cdb, mod); err != nil {
+			warnf("update: error upgrading mod %v: %v\n", mod, err)
+		}
+	}
+	return nil
+}
+
+func updateMod(cdb *contentdb.Client, modDir string) error {
+	yellow("* Checking mod dir ./%v ", modDir)
+	pwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if err := os.Chdir(modDir); err != nil {
+		return err
+	}
+	defer os.Chdir(pwd)
+
+	_, err = os.Stat(".git")
+	if err == nil {
+		fmt.Printf("Mod managed using git. Running `git pull`...\n")
+		// .git exists, upgrade from upstream
+		b, err := exec.Command("git", "pull").CombinedOutput()
+		if err != nil {
+			warnf("  updateMod: error executing `git pull`: %v", err)
+		}
+		green("* Git output:\n%s\n", string(b))
+		return err
+	}
+
+	// Check if it's a mod
+	var err1 error
+	b, err := ioutil.ReadFile("mod.conf")
+	if err != nil {
+		// Check if it's a modpack
+		b, err1 = ioutil.ReadFile("modpack.conf")
+		if err1 != nil {
+			return fmt.Errorf("upgradeMod: unable to load configuration (mod=%v; modpack=%v)", err, err1)
+		}
+	}
+
+	// Check local version
+	modconf, err := ini.Load(b)
+	if err != nil {
+		return err
+	}
+	cfg := modconf.Section("")
+	author := cfg.Key("author").String()
+	name := cfg.Key("name").String()
+	release, _ := cfg.Key("release").Int64()
+	fmt.Printf("Installed version %v/%v@%v, ", author, name, release)
+
+	// Check remote version
+	pkg, err := cdb.GetPackage(author, name)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("remote version %v/%v@%v: ", pkg.Author, pkg.Name, pkg.Release)
+
+	// Update?
+	if int64(pkg.Release) > release && release > 0 {
+		fmt.Printf("updating to version %v ...\n", pkg.Release)
+		os.Chdir(pwd)
+		return installMod([]string{fmt.Sprintf("%s/%s", author, name)})
+	}
+	fmt.Println("package is already up to date.")
 	return nil
 }
